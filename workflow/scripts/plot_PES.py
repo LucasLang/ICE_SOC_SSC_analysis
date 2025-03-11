85cca04341009419099aecc5287f8462f82d8e42
Lucas Lang <lucas.lang@chem.tu-berlin.de>
2025-03-11 12:18:44 +0100

    PES plot added to workflow.

--- workflow/scripts/plot_PES.py
+++ workflow/scripts/plot_PES.py
@@ -1,49 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import UnivariateSpline
+import sys
+
+expfolder = sys.argv[1]
+transitionenergy_file = sys.argv[2]
+outfolder = sys.argv[3]
 
 GS_min = -0.08251407089831986     # this is the lowest energy found in data_X
-ICE_E = np.loadtxt("ICE_transition_energies") + GS_min        # align the two energy scales
-r_min = 1.2058   # the distance I took from NIST
+ICE_E = np.loadtxt(transitionenergy_file) + GS_min        # align the two energy scales
+r_min = 1.2058   # the O-O distance I used in the calculations
 
 
-data_X = np.loadtxt("data_X.csv", delimiter=",")
+data_X = np.loadtxt(f"{expfolder}/data_X.csv", delimiter=",")
 spline_X = UnivariateSpline(data_X[:,0], data_X[:,1], s=0.005)
 spline_X_v = np.vectorize(lambda x : spline_X(x))
 grid_X = np.linspace(data_X[0,0], data_X[-1,0], 200)
 values_X = spline_X_v(grid_X)
 
-data_a = np.loadtxt("data_a.csv", delimiter=",")
+data_a = np.loadtxt(f"{expfolder}/data_a.csv", delimiter=",")
 spline_a = UnivariateSpline(data_a[:,0], data_a[:,1], s=0.005)
 spline_a_v = np.vectorize(lambda x : spline_a(x))
 grid_a = np.linspace(data_a[0,0], data_a[-1,0], 200)
 values_a = spline_a_v(grid_a)
 
-data_b = np.loadtxt("data_b.csv", delimiter=",")
+data_b = np.loadtxt(f"{expfolder}/data_b.csv", delimiter=",")
 spline_b = UnivariateSpline(data_b[:,0], data_b[:,1], s=0.005)
 spline_b_v = np.vectorize(lambda x : spline_b(x))
 grid_b = np.linspace(data_b[0,0], data_b[-1,0], 200)
 values_b = spline_b_v(grid_b)
 
-data_c = np.loadtxt("data_c.csv", delimiter=",")
+data_c = np.loadtxt(f"{expfolder}/data_c.csv", delimiter=",")
 spline_c = UnivariateSpline(data_c[:,0], data_c[:,1], s=0.005)
 spline_c_v = np.vectorize(lambda x : spline_c(x))
 grid_c = np.linspace(data_c[0,0], data_c[-1,0], 200)
 values_c = spline_c_v(grid_c)
 
-data_C = np.loadtxt("data_C.csv", delimiter=",")
+data_C = np.loadtxt(f"{expfolder}/data_C.csv", delimiter=",")
 spline_C = UnivariateSpline(data_C[:,0], data_C[:,1], s=0.005)
 spline_C_v = np.vectorize(lambda x : spline_C(x))
 grid_C = np.linspace(data_C[0,0], data_C[-1,0], 200)
 values_C = spline_C_v(grid_C)
 
-data_A = np.loadtxt("data_A.csv", delimiter=",")
+data_A = np.loadtxt(f"{expfolder}/data_A.csv", delimiter=",")
 spline_A = UnivariateSpline(data_A[:,0], data_A[:,1], s=0.005)
 spline_A_v = np.vectorize(lambda x : spline_A(x))
 grid_A = np.linspace(data_A[0,0], data_A[-1,0], 200)
 values_A = spline_A_v(grid_A)
 
-data_B = np.loadtxt("data_B.csv", delimiter=",")
+data_B = np.loadtxt(f"{expfolder}/data_B.csv", delimiter=",")
 spline_B = UnivariateSpline(data_B[:,0], data_B[:,1], s=0.005)
 spline_B_v = np.vectorize(lambda x : spline_B(x))
 grid_B = np.linspace(data_B[0,0], data_B[-1,0], 200)
@@ -65,4 +70,4 @@ plt.xlabel('Internuclear distance / \u212b')
 plt.ylabel('Energy / eV')
 plt.subplots_adjust(bottom=0.15)
 plt.legend()
-plt.savefig('plot.png', dpi=300)
+plt.savefig(f"{outfolder}/PES.png", dpi=300)
