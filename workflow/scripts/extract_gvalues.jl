6e93989348aa026fb810865c977724fa633f7a45
Lucas Lang <lucas.lang@chem.tu-berlin.de>
2025-05-12 14:36:05 +0200

    Modified extract_gvalues.jl in order to allow for NEVPT2 diagonal energies.

--- workflow/scripts/extract_gvalues.jl
+++ workflow/scripts/extract_gvalues.jl
@@ -2,9 +2,10 @@ using OutputParser
 using DelimitedFiles
 
 ORCAfile = ARGS[1]
-outfile = ARGS[2]
+method = ARGS[2]
+outfile = ARGS[3]
 
-values = [parse_float(ORCAfile, ["KRAMERS PAIR", "g-factors"], 1, word) for word in 0:2]
+values = [parse_float(ORCAfile, ["QDPT WITH $method DIAGONAL ENERGIES", "KRAMERS PAIR", "g-factors"], 1, word) for word in 0:2]
 values_rounded = [round(value, digits=2) for value in values]
 
 writedlm(outfile, values_rounded)
